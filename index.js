06408c3121ac3c8d050c5c40026c53ba92588d46
YaolinTan <yaolint@nycstudents.net>
2024-09-27 12:22:25 -0400

    e

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("hello world");
+console.log("hello world hehe");
+function canOpenRoam(isHerbivore, weight, isNotPredator) {
+  // Check the conditions based on the truth map
+  if (isHerbivore && weight < 50 && isNotPredator) {
+    return {"Allowed to open roam"};
+  } else if (!isHerbivore && weight < 50 && isNotPredator) {
+    return {"Allowed to open roam"};
+  } else if (!isHerbivore && weight >= 50 && isNotPredator) {
+    return false;
+  } else if (!isHerbivore && weight >= 50 && !isNotPredator) {
+    return false;
+  } else if (isHerbivore && weight >= 50 && isNotPredator) {
+    return {"Allowed to open roam"};
+  } else if (isHerbivore && weight >= 50 && !isNotPredator) {
+    return false;
+  } else if (isHerbivore && weight < 50 && !isNotPredator) {
+    return false;
+  }
+  // Default case
+  return false;
+}
+
+console.log(canOpenRoam(true, 30, true));
