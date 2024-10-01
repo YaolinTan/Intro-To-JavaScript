ec20591552e4847e6068e09a693b21fb4a88b4d0
YaolinTan <yaolint@nycstudents.net>
2024-10-01 00:08:25 -0400

    e

--- park.js
+++ park.js
@@ -1,9 +1,5 @@
 console.log("hello world");
 function discount(age, resident, member) {
-  if ((12 < age < 65, resident || member)) {
-    console.log("Yes");
-  } else {
-    console.log("No");
-  }
+  (12 < age < 65, resident || member) ? console.log("Yes") : console.log("No");
 }
-discount(70, true, false);
+discount(30, false, false);
